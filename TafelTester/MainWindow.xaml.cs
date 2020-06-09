b1da058d3ab2ec4e7420bdea75187dabc0a49390
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2020-06-09 15:54:09 +0200

    nu met half werkende shuffle

--- TafelTester/MainWindow.xaml.cs
+++ TafelTester/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Converters;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,6 +21,9 @@ namespace TafelTester
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random rnd = new Random();
+        private List<int> reeks = new List<int>();
+        //private List<int> sommen;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,24 +32,53 @@ namespace TafelTester
 
         }
 
-        private void tbSom5_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void tbSom2_TextChanged(object sender, TextChangedEventArgs e)
+        private StackPanel CreateSom(int tafel, int random)
         {
+            TextBox tbAntwoord = new TextBox();
+            tbAntwoord.Width = 150;
+            Label lbSom = new Label();
+            lbSom.Content = tafel + " x " + random;
+            Label lbGoedFout = new Label();
+            lbGoedFout.Content = "Goed";
+            lbGoedFout.Visibility = Visibility.Visible;
 
+            StackPanel stp = new StackPanel();
+            stp.Orientation = Orientation.Horizontal;
+            stp.Children.Add(lbSom);
+            stp.Children.Add(tbAntwoord);
+            stp.Children.Add(lbGoedFout);
+
+            wpSommen.Children.Add(stp);
+
+            return stp;
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
 
+        private void MaakSommen(object sender, RoutedEventArgs e)
+        {
+            //vullen
+            for (int j = 0; j < 10; j++)
+            {
+                reeks.Add(j + 1);
+            }
+
+            //shuffle
+            for (int i = 0; i < reeks.Count; i++)
+            {
+                int k = rnd.Next(i + 1);
+                int value = reeks[k];
+                reeks[k] = reeks[i];
+                reeks[i] = value;
+
+                CreateSom(Convert.ToInt32(tbTafel.Text), reeks[i]);
+            }
+       
         }
     }
 }
